1fe93caf86d7334a7539a0e706220ecdb6075f8e
DajanaD <117541907+DajanaD@users.noreply.github.com>
2023-11-25 16:12:04 +0200

    додавання итератора main.py

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,20 @@
 from collections import UserDict
+import datetime
 
 
 class Field:
     def __init__(self, value):
+        self.__value = None
         self.value = value
+ 
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.__value = value
+
     def __str__(self):
         return str(self.value)
 
@@ -11,14 +22,20 @@ class Field:
 class Name(Field):
     pass
 
+class Birthday(Field):
+    @Field.value.setter
+    def value(self, value: str):
+        self.__value = datetime.strptime(value, '%Y.%m.%D').date()
 
 class Phone(Field):
    def __init__(self, value):
         super().__init__(value)
-        if len(str(self.value)) == 10 and str(self.value).isdigit():
-            self.value = value
-        else:
-            raise ValueError('Invalid phone number')
+        @Field.value.setter
+        def value(self, value: str):
+            if len(str(self.value)) == 10 and str(self.value).isdigit():
+                self.value = value
+            else:
+                raise ValueError('Invalid phone number')
 
 class Record:
     def __init__(self, name):
@@ -48,6 +65,16 @@ class Record:
         if phone:
             self.phones.remove(phone)
 
+    def days_to_birthday(self, birthday):
+        if birthday:
+            now = datetime.datetime.now()
+            then = datetime.datetime.strptime(birthday, '%Y.%m.%D')
+            delta1 = datetime.datetime(now.year, then.month, then.day)
+            delta2 = datetime.datetime(now.year+1, then.month, then.day)
+
+            result = ((delta1 if delta1 > now else delta2) - now).days
+            print(f'{result} days left until birthday')
+
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
@@ -65,3 +92,14 @@ class AddressBook(UserDict):
     def delete(self, name:Record):
         if self.data.get(name):
             del self.data[name]
+
+    def iterator(self, item_number):
+        counter = 0
+        result = ''
+        for item, record in self.data.items():
+            result += f'{item}: {record}'
+            counter += 1
+            if counter >= item_number:
+                yield result
+                counter = 0
+                result = '' 
